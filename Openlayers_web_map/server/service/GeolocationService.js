8f1b705653e3800b42a8c5c8c1231ebd0f32732d
volamthienphu <phuvo@hitek.com.vn>
2024-06-01 20:21:18 +0700

    change code

--- Openlayers_web_map/server/service/GeolocationService.js
+++ Openlayers_web_map/server/service/GeolocationService.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const City = require("../model/City");
 const path = require("path");
 const axios = require("axios");
+const { sequelize } = require("../models");
 
 const geojsonFilePath = `${path.join(__dirname, "../data/map.geojson")}`;
 const geojson = JSON.parse(fs.readFileSync(geojsonFilePath, "utf8"));
@@ -14,26 +15,32 @@ async function saveGeoJSONToDatabase() {
       const { ID, Cityname, Cityimage, description } = feature.properties;
       const coordinates = feature.geometry.coordinates;
       console.log("🚀 ~ saveGeoJSONToDatabase ~ coordinates:", coordinates);
+      const transaction = await sequelize.transaction();
 
-      const [exist, newModel] = await City.findOrCreate({
-        where: {
-          ID,
+      const [exist, newModel] = await City.findOrCreate(
+        {
+          where: {
+            ID,
+          },
+          defaults: {
+            ID,
+            Cityname,
+            Cityimage,
+            location: { type: "Point", coordinates },
+            description,
+          },
         },
-        defaults: {
-          ID,
-          Cityname,
-          Cityimage,
-          location: { type: "Point", coordinates },
-          description,
-        },
-      });
+        { transaction }
+      );
       if (exist) {
-        await exist.update({ total_click_count: 0 });
+        await exist.update({ total_click_count: 0 }, { transaction });
       }
     }
     console.log("GeoJSON data has been saved to the database.");
+    await transaction.commit();
   } catch (error) {
     console.error("Error saving GeoJSON data:", error);
+    await transaction.rollback();
   }
 }
 
