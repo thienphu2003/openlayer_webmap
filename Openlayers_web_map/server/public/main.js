e9993dad9990f3a7dafb9484b5817cc92c692bd2
volamthienphu <phuvo@hitek.com.vn>
2024-06-01 18:25:40 +0700

    change port

--- Openlayers_web_map/server/public/main.js
+++ Openlayers_web_map/server/public/main.js
@@ -2,7 +2,7 @@ window.onload = init;
 
 const totalCountClick = 0;
 
-const SERVER_HOST = "http://ec2-3-233-122-181.compute-1.amazonaws.com:3002";
+const SERVER_HOST = "http://ec2-3-233-122-181.compute-1.amazonaws.com";
 
 const IMAGE_PRODUCTION_URL =
   "https://kietphuhuybucket.s3.amazonaws.com/City_images/";
