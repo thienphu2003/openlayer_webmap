6d47bfbb5dbf15e2ceb8cc285c118e3bd377d24e
volamthienphu <phuvo@hitek.com.vn>
2024-06-01 19:42:36 +0700

    change code

--- Openlayers_web_map/server/service/GeolocationService.js
+++ Openlayers_web_map/server/service/GeolocationService.js
@@ -25,6 +25,8 @@ async function saveGeoJSONToDatabase() {
           Cityimage,
           location: { type: "Point", coordinates },
           description,
+          total_click_count: 0,
+          last_time_click: Date.now(),
         },
       });
       if (exist) {
