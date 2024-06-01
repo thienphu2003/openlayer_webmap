84c3c6f3c7fb3a1fcd782ee2112d045e255e02c0
volamthienphu <phuvo@hitek.com.vn>
2024-06-01 21:34:56 +0700

    change code

--- Openlayers_web_map/server/service/GeolocationService.js
+++ Openlayers_web_map/server/service/GeolocationService.js
@@ -7,24 +7,22 @@ const axios = require("axios");
 const { Sequelize } = require("../models");
 const { Op } = require("sequelize");
 
-// const geojsonFilePath = `${path.join(__dirname, "../data/map.geojson")}`;
-// const geojson = JSON.parse(fs.readFileSync(geojsonFilePath, "utf8"));
-
 async function saveGeoJSONToDatabase() {
   try {
-    await City.update(
-      {
-        total_click_count: 0,
-      },
-      {
-        where: {
-          total_click_count: {
-            [Op.gt]: 0,
-          },
+    await Promise.all([
+      City.update(
+        {
+          total_click_count: 0,
         },
-      }
-    );
-
+        {
+          where: {
+            total_click_count: {
+              [Op.gt]: 0,
+            },
+          },
+        }
+      ),
+    ]);
     console.log("GeoJSON data has been saved to the database.");
   } catch (error) {
     console.error("Error saving GeoJSON data:", error);
