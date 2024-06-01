5c6556e1775eb719a22c7ad54dfeabce8d37786e
volamthienphu <phuvo@hitek.com.vn>
2024-06-01 21:51:01 +0700

    final version

--- Openlayers_web_map/server/public/main.js
+++ Openlayers_web_map/server/public/main.js
@@ -164,35 +164,35 @@ function init() {
       cityImageElement.setAttribute("src", IMAGE_PRODUCTION_URL + featureImage);
       cityDescription.innerHTML = featureDescription;
       // totalClickCount.innerHTML = totalCountClick;
-      const data = {
-        ID: feature.get("ID"),
-      };
-      const response = await fetch(SERVER_HOST, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+      // const data = {
+      //   ID: feature.get("ID"),
+      // };
+      // const response = await fetch(SERVER_HOST, {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(data),
+      // });
 
-      const result = await response.json();
-      const date = new Date(result.time);
-      const formatter = new Intl.DateTimeFormat("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        timeZone: "Asia/Ho_Chi_Minh",
-      });
-      const formattedDate = formatter.format(date);
-      console.log(formattedDate);
-      totalClickCount.innerHTML =
-        "Total Click Count " +
-        result?.count +
-        " and last time click  " +
-        formattedDate;
+      // const result = await response.json();
+      // const date = new Date(result.time);
+      // const formatter = new Intl.DateTimeFormat("en-US", {
+      //   year: "numeric",
+      //   month: "2-digit",
+      //   day: "2-digit",
+      //   hour: "2-digit",
+      //   minute: "2-digit",
+      //   second: "2-digit",
+      //   timeZone: "Asia/Ho_Chi_Minh",
+      // });
+      // const formattedDate = formatter.format(date);
+      // console.log(formattedDate);
+      // totalClickCount.innerHTML =
+      //   "Total Click Count " +
+      //   result?.count +
+      //   " and last time click  " +
+      //   formattedDate;
     }
   }
 
