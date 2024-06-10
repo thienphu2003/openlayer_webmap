34cc4df39f8cad39ff9d763a7f551cde04f7a9d4
volamthienphu <phuvo@hitek.com.vn>
2024-06-10 11:02:49 +0700

    UPDATE: update code

--- Openlayers_web_map/server/service/GeolocationService.js
+++ Openlayers_web_map/server/service/GeolocationService.js
@@ -16,7 +16,7 @@ async function saveGeoJSONToDatabase() {
       const coordinates = feature.geometry.coordinates;
       console.log("🚀 ~ saveGeoJSONToDatabase ~ coordinates:", coordinates);
       const t = await sequelize.transaction();
-      await City.truncate({ transaction });
+      await City.truncate({ t });
 
       const newRecords = await City.create(
         {
@@ -27,14 +27,14 @@ async function saveGeoJSONToDatabase() {
           description,
           total_click_count: 0,
         },
-        { transaction }
+        { t }
       );
     }
-    await transaction.commit();
+    await t.commit();
     console.log("GeoJSON data has been saved to the database.");
   } catch (error) {
     console.error("Error saving GeoJSON data:", error);
-    await transaction.rollback();
+    await t.rollback();
   }
 }
 
